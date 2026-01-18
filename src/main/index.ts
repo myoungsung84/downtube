70511d3913688ca72069e16e103bd7e039aa8f07
bms <myoungsung84@gmail.com>
2026-01-19 08:25:46 +0900

    security: enable sandbox and contextIsolation for main window

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,7 +13,8 @@ function createWindow(): void {
     autoHideMenuBar: true,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: true,
+      contextIsolation: true
     },
     icon: app.isPackaged
       ? join(process.resourcesPath, 'assets', 'icon.png')
