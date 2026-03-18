332363f7018b5a793e4568a9f57456573f2f5cce
bms <myoungsung84@gmail.com>
2026-03-18 10:38:21 +0900

    feat: 공용 dialog provider 추가

--- src/renderer/app/shared/providers/app-providers.tsx
+++ src/renderer/app/shared/providers/app-providers.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import DialogProvider from './dialog/dialog-provider'
 import ToastProvider from './toast/toast-provider'
 
 export default function AppProviders(props: { children: React.ReactNode }): React.JSX.Element {
-  return <ToastProvider>{props.children}</ToastProvider>
+  return (
+    <ToastProvider>
+      <DialogProvider>{props.children}</DialogProvider>
+    </ToastProvider>
+  )
 }
